e6d34200f31a954dbc632acc69b30afef316b47a
Rudney R <rudney.un2016@gmail.com>
2020-10-18 17:10:45 -0300

    Upload files day 2

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,46 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+}
+
+// Create map
+const map = L.map("mapid", options).setView([-9.3899564, -40.5015384], 14);
+
+//Create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+//Create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+//Create and add marker
+L.marker([-9.3899564, -40.5015384], { icon }).addTo(map);
+
+/*Image Gallery */
+
+function selectimage(event) {
+    const button = event.currentTarget
+
+    // Remover todas as classes .active
+    const buttons = document.querySelectorAll('.images button')
+    buttons.forEach((button) => {
+        button.classList.remove("active")
+    })
+    
+    //Selecionar a imagem clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector('.orphanage-details > img')
+
+    //Atualizar o container de imagem
+    imageContainer.src = image.src
+
+    //Adicionar a classe .active para o borão clicado
+    button.classList.add('active')
+}
